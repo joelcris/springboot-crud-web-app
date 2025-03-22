44a344bd3a4be61578959695bbfdfad5aee0f3a3
joelcris <joel.crisanto.juarez@gmail.com>
2025-03-22 02:37:55 -0600

    add basic spring boot src

---
+++ src/main/java/com/webapp/springboot_crud_web_app/SpringbootCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.springboot_crud_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapp/springboot_crud_web_app/SpringbootCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.springboot_crud_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
